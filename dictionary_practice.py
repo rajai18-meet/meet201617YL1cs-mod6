e7cac942bf711d7fc7a2b6e940ca5ebcc78d68f7
rajai18-meet <kingdom-wizard@hotmail.com>
2017-03-06 19:59:01 +0200

    phonebook

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -6,7 +6,7 @@ my_phonebook={"Statue of Liberty":2125555555, "Ghostbusters":2125551234}
 ###
 #Assign the value of the Statue of Liberty's phone number to the variable, num
 ###
-
+my_phonebook["Statue of Liberty"]
 ###
 #Print the variable, num
 ###
@@ -17,6 +17,13 @@ my_phonebook={"Statue of Liberty":2125555555, "Ghostbusters":2125551234}
 
 #Here's an empty dictionary
 your_phonebook={}
+your_phonebook['Rajai']= 01234
+your_phonebook['fouad']= 012345
+your_phonebook['hadar']= 0123456
+your_phonebook['nisreen']= 01234567
+your_phonebook['mikella']= 012345678
+
+your_phonebook.keys()
 
 ###
 #Add 5 values to it like this
